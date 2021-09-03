8c2edab8e1ef88658e201de9eb4a65f7542dfc33
YOUNGJOO-YOON <zerozoo-front@gmail.com>
2021-09-03 17:38:33 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,111 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
+import { gql, useQuery } from '@apollo/client'
+
+
+
+
+  // const EXCHANGE_RATES = gql`
+  // query GetExchanges{
+  //   rates(currency:"USD"){
+  //   rate
+  //   currency
+  //   }
+  // }`
+
+  // function ExchangeRates({currency, rate}){
+  //   const { loading, error, data } = useQuery(EXCHANGE_RATES,{
+  //     variables:{currency, rate}
+  //   })
+  //   if(loading) return <p>Loading...</p>
+  //   if(error) return <p>Error...</p>
+
+
+  //   console.log('data: ',data)
+  //   return(
+  //     <select>
+  //       {data.rates.map(({currency, rate})=>(
+  //         <option key={currency}>
+  //           {currency}: {rate}
+  //         </option>
+  //     ))}
+  //   </select>
+  //   )
+
+  //   // return data?.rates?.map()
+  // }
+const GET_DOGS = gql`
+query GetDogs {
+  dogs {
+    id
+    breed
+  }
+}
+`
+
+function Dogs({ onDogSelected }) {
+  const { loading, error, data } = useQuery(GET_DOGS);
+
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+
+  return (
+    <select name="dog" onChange={onDogSelected}>
+      {data.dogs.map(dog => (
+        <option key={dog.id} value={dog.breed}>
+          {dog.breed}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+const GET_DOG_PHOTO = gql`
+  query dog($breed: String!){
+    dog(breed: $breed){
+      id
+      displayImage
+    }
+  }
+`
+
+function DogPhoto({ breed }){
+  const { loading, error, data, refetch, networkStatus } = useQuery(
+    GET_DOG_PHOTO,
+    {
+      variables: { breed },
+      notifyOnNetworkStatusChange: true
+    }
+  )
+
+  if (networkStatus === 4 ) return <p>Refetching!</p>
+  if (loading) return null
+  if (error) return `Error!: ${error}`
+  return(
+    <div>
+        <div>
+          <img src={data.dog.displayImage} stype={{height:100, width:100}} />
+        </div>
+        <button onClick={()=>refetch()}>Refetch!</button>
+    </div>
+  )
+}
+
 
 function App() {
+
+  const [selectedDog, setSelectedDog] = useState(null)
+
+
+  function onDogSelected({target:{value}}){
+    setSelectedDog(value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App-header'>
+      <h2>hello </h2>
+      {selectedDog && <DogPhoto breed = {selectedDog} />}
+      <Dogs onDogSelected={onDogSelected}/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from '@apollo/client' 
 
+const client = new ApolloClient({
+  // uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  uri: 'https://71z1g.sse.codesandbox.io/',
+  cache: new InMemoryCache()
+})
 ReactDOM.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+  </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
